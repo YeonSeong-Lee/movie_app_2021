a0f06cb439f604d6bd4a5eb90918975ddebd8f47
YeonSeong-Lee <62806979+YeonSeong-Lee@users.noreply.github.com>
2021-04-11 02:28:28 +0900

    최종 완성 코드

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css'
+
+function About() {
+    return (
+    <div className = "about__container">
+    
+    <span>"Done is better than perfect" </span>
+    </div>
+    );
+}
+
+export default About;
